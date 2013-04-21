7080c713f40544e6b353db0eb35abfb48f2ad47e
Luis Fernandes <zipleen@gmail.com>
2013-04-21 15:54:43 +0100

    reset data after 15 min, if app goes to background

--- TvhClient/TVHAppDelegate.m
+++ TvhClient/TVHAppDelegate.m
@@ -29,7 +29,9 @@
 #import "GAI.h"
 #endif
 
-@implementation TVHAppDelegate
+@implementation TVHAppDelegate {
+    NSDate *exitTime;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -63,6 +65,7 @@
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    exitTime = [NSDate date];
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -80,6 +83,10 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    // after 15 min we to reset settings so we fetch new data
+    if ( [[NSDate date] compare:[exitTime dateByAddingTimeInterval:1800]] == NSOrderedDescending ) {
+        [[TVHSettings sharedInstance] resetSettings];
+    }
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
