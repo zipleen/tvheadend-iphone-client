1a865960015e553221a255ee0699e3b5922e51be
Luis Fernandes <zipleen@gmail.com>
2013-05-19 21:17:56 +0100

    updated test flight

--- TestFlight SDK/TestFlight.h
+++ TestFlight SDK/TestFlight.h
@@ -6,16 +6,12 @@
 //  Copyright 2011 TestFlight. All rights reserved.
 
 #import <Foundation/Foundation.h>
-#define TESTFLIGHT_SDK_VERSION @"1.3.0-beta.2"
+#define TESTFLIGHT_SDK_VERSION @"1.2.5-beta"
 #undef TFLog
 
 #if __cplusplus
 extern "C" { 
 #endif
-    /*
-     * Remote Logging
-     * Note: All Logging is synchronous, see the README for more information.
-     */
     void TFLog(NSString *format, ...);
     void TFLogv(NSString *format, va_list arg_list);
 #if __cplusplus
@@ -26,7 +22,9 @@ extern "C" {
  * TestFlight object
  * All methods are class level
  */
-@interface TestFlight : NSObject
+@interface TestFlight : NSObject {
+
+}
 
 /**
  * Add custom environment information
@@ -45,6 +43,7 @@ extern "C" {
  *                         The token for this application can be retrieved by going to https://testflightapp.com/dashboard/applications/
  *                         selecting this application from the list then selecting SDK.
  */
+
 + (void)takeOff:(NSString *)applicationToken;
 
 /**
@@ -56,19 +55,16 @@ extern "C" {
 + (void)setOptions:(NSDictionary*)options;
 
 /**
- * Track when a user has passed a checkpoint after the flight has taken off. Eg. passed level 1, posted high score.
- * Checkpoints are sent in the background.
- * Note: The checkpoint is logged synchronously (See TFLog and TFOptionLogOnCheckpoint for more information).
+ * Track when a user has passed a checkpoint after the flight has taken off. Eg. passed level 1, posted high score
  *
  * @param checkpointName The name of the checkpoint, this should be a static string
  */
 + (void)passCheckpoint:(NSString *)checkpointName;
 
 /**
- * Use to manually flush data to TestFlight.
- * TestFlight automatically flushes at the end of a session and every `TFOptionFlushSecondsInterval`.
+ * Opens a feedback window that is not attached to a checkpoint
  */
-+ (void)flush;
++ (void)openFeedbackView;
 
 /**
  * Submits custom feedback to the site. Sends the data in feedback to the site. This is to be used as the method to submit
@@ -109,9 +105,8 @@ extern "C" {
  * Pass these as keys to the dictionary you pass to +`[TestFlight setOptions:]`.
  * The values should be NSNumber BOOLs (`[NSNumber numberWithBool:YES]` or `@YES`)
  */
+extern NSString *const TFOptionAttachBacktraceToFeedback; // Defaults to @NO. Setting to @YES attaches the current backtrace, with symbols, to the feedback.
 extern NSString *const TFOptionDisableInAppUpdates; // Defaults to @NO. Setting to @YES, disables the in app update screen shown in BETA apps when there is a new version available on TestFlight.
-extern NSString *const TFOptionFlushSecondsInterval; // Defaults to @60. Set to a number. @0 turns off the flush timer.
-extern NSString *const TFOptionLogOnCheckpoint; // Defaults to @YES. Because logging is synchronous, if you have a high preformance app, you might want to turn this off.
 extern NSString *const TFOptionLogToConsole; // Defaults to @YES. Prints remote logs to Apple System Log.
 extern NSString *const TFOptionLogToSTDERR; // Defaults to @YES. Sends remote logs to STDERR when debugger is attached.
 extern NSString *const TFOptionReinstallCrashHandlers; // If set to @YES: Reinstalls crash handlers, to be used if a third party library installs crash handlers overtop of the TestFlight Crash Handlers.
