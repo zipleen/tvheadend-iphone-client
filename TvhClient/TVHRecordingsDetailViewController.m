a9fb7eae3c813bac841f395da4614b4a694055e8
Luis Fernandes <zipleen@gmail.com>
2013-10-23 22:40:57 +0100

    fix label length in program detail

--- TvhClient/TVHRecordingsDetailViewController.m
+++ TvhClient/TVHRecordingsDetailViewController.m
@@ -271,7 +271,7 @@
     CGSize size = [descLabel.text
                    sizeWithFont:descLabel.font
                    constrainedToSize:CGSizeMake(screenWidth-40, CGFLOAT_MAX)];
-    descLabel.frame = CGRectMake(20, 20, size.width+2, size.height + self.fontSizeHack);
+    descLabel.frame = CGRectMake(20, 20, screenWidth-40, size.height + self.fontSizeHack);
     
     if ( ! DEVICE_HAS_IOS7 ) {
         // line separator
