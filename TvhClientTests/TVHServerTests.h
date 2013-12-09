9a6cc2a61f9f2eb76cf0bda4a038a0d24b14e9f0
Luis Fernandes <zipleen@gmail.com>
2013-12-09 00:45:33 +0000

    renamed nickname

--- TvhClientTests/TVHServerTests.h
+++ TvhClientTests/TVHServerTests.h
@@ -2,8 +2,8 @@
 //  TVHServerTests.h
 //  TvhClient
 //
-//  Created by zipleen on 7/21/13.
-//  Copyright (c) 2013 zipleen. All rights reserved.
+//  Created by Luis Fernandes on 7/21/13.
+//  Copyright (c) 2013 Luis Fernandes. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
--- TvhClientTests/TVHTagStoreTests.m
+++ TvhClientTests/TVHTagStoreTests.m
@@ -2,7 +2,7 @@
 //  TVHTagStoreTests.m
 //  TVHeadend iPhone Client
 //
-//  Created by zipleen on 2/22/13.
+//  Created by Luis Fernandes on 2/22/13.
 //  Copyright 2013 Luis Fernandes
 //
 //  This Source Code Form is subject to the terms of the Mozilla Public
