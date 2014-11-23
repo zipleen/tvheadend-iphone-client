08e9fd607409208a127b245f5f966d035a53c042
Luis Fernandes <zipleen@gmail.com>
2014-11-23 19:01:20 +0000

    missing speedcategory from vlc which was pointing to outside of the project.

---
+++ TvhClient/vlc/UIDevice+SpeedCategory.h
@@ -0,0 +1,19 @@
+/*****************************************************************************
+ * UIDevice+SpeedCategory.h
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2013 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Felix Paul Kühne <fkuehne # videolan.org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (SpeedCategory)
+
+- (int)speedCategory;
+
+@end
